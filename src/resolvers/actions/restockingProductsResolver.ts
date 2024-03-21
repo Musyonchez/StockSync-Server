d6edb3dc79ae9dcf58112435c172a2d18b57762f
Musyonchez <musyonchez@gmail.com>
2024-03-21 16:07:36 +0300

    remove                           firstRecordAction: true,

--- src/resolvers/actions/restockingProductsResolver.ts
+++ src/resolvers/actions/restockingProductsResolver.ts
@@ -94,7 +94,6 @@ export const restockingProductResolver = {
                           current: {
                             increment: numberToAdd,
                           },
-                          firstRecordAction: true,
                         },
                       });
 
